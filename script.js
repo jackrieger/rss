7eb59a90193abd61176bf36e168f872cfcd37ba1
Jack Rieger <jack@jackrieger.com>
2024-02-28 23:11:42 +0000

    Style

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // CORS proxy
   const proxy = 'https://corsproxy.io/?';
-
-  // Feed URLs
   const feedUrls = [
-    proxy + encodeURIComponent('https://hnrss.org/frontpage.atom'),
-    proxy + encodeURIComponent('https://world.hey.com/dhh/feed.atom')
+    {url: proxy + encodeURIComponent('https://hnrss.org/frontpage.atom'), className: "hacker-news"},
+    {url: proxy + encodeURIComponent('https://world.hey.com/dhh/feed.atom'), className: "dhh-feed"}
   ];
 
-  // Fetch feeds
-  feedUrls.forEach(url => fetchAndParseFeed(url));
+  feedUrls.forEach(feed => fetchAndParseFeed(feed.url, feed.className));
 });
 
-function fetchAndParseFeed(feedUrl) {
+function fetchAndParseFeed(feedUrl, className) {
   fetch(feedUrl)
     .then(response => {
       if (!response.ok) {
@@ -23,16 +19,15 @@ function fetchAndParseFeed(feedUrl) {
     .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
     .then(data => {
       const items = data.querySelectorAll("entry");
-      let content = document.querySelector("#feed-content");
+      let column = document.querySelector(`.${className}`);
 
       items.forEach(item => {
         const title = item.querySelector("title").textContent;
         const linkElement = item.querySelector("link");
         const link = linkElement ? linkElement.getAttribute("href") : null;
 
-        // Append to content div, if link is present
         if(link) {
-          content.innerHTML += `<p><a href="${link}" target="_blank">${title}</a></p>`;
+          column.innerHTML += `<div class="card"><a href="${link}" target="_blank">${title}</a></div>`;
         }
       });
     })
